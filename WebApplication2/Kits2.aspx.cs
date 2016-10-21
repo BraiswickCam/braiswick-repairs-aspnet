d4a431557910186b18ee1ff26fbeb33764f0be3b
Alex <alexbass666@gmail.com>
2016-10-21 15:41:04 +0100

    Tweak logic and add SQL for repairs reporting

--- WebApplication2/Kits2.aspx.cs
+++ WebApplication2/Kits2.aspx.cs
@@ -139,13 +139,13 @@ namespace WebApplication2
 
             string kitID = DropDownList1.SelectedValue;
 
-            photogAddRemove.HRef = details.Rows[0][2].ToString() != string.Empty ? String.Format("Kits2.aspx?KitID={0}&type=Photog&ID=0", kitID) : String.Format("Photogs.aspx?KitID={0}", kitID);
+            photogAddRemove.HRef = details.Rows[0][2].ToString() != string.Empty ? String.Format("Kits2.aspx?KitID={0}&type=remPhotog&ID={1}", kitID, details.Rows[0][2].ToString()) : String.Format("Photogs.aspx?KitID={0}", kitID);
             photogAddRemove.InnerText = details.Rows[0][2].ToString() != string.Empty ? "Remove Photographer" : "Add Photographer";
 
-            cameraAddRemove.HRef = details.Rows[0][6].ToString() != string.Empty ? String.Format("Kits2.aspx?KitID={0}&type=Camera&ID=0", kitID) : String.Format("Equipment.aspx?KitID={0}&type=Camera", kitID);
+            cameraAddRemove.HRef = details.Rows[0][6].ToString() != string.Empty ? String.Format("Kits2.aspx?KitID={0}&type=remCamera&ID={1}", kitID, details.Rows[0][6].ToString()) : String.Format("Equipment.aspx?KitID={0}&type=Camera", kitID);
             cameraAddRemove.InnerText = details.Rows[0][6].ToString() != string.Empty ? "Remove Camera" : "Add Camera";
 
-            laptopAddRemove.HRef = details.Rows[0][10].ToString() != string.Empty ? String.Format("Kits2.aspx?KitID={0}&type=Laptop&ID=0", kitID) : String.Format("Equipment.aspx?KitID={0}&type=Laptop", kitID);
+            laptopAddRemove.HRef = details.Rows[0][10].ToString() != string.Empty ? String.Format("Kits2.aspx?KitID={0}&type=remLaptop&ID={1}", kitID, details.Rows[0][10].ToString()) : String.Format("Equipment.aspx?KitID={0}&type=Laptop", kitID);
             laptopAddRemove.InnerText = details.Rows[0][10].ToString() != string.Empty ? "Remove Laptop" : "Add Laptop";
         }
 
@@ -160,23 +160,31 @@ namespace WebApplication2
             int itemID;
             try { itemID = Convert.ToInt32(Request.QueryString["ID"]); }
             catch (System.FormatException) { return false; }
+            int oldID = itemID;
             int kitID;
             try { kitID = Convert.ToInt32(Request.QueryString["KitID"]); }
             catch (System.FormatException) { return false; }
 
-            if (type == "Laptop") type = "LaptopID";
-            else if (type == "Camera") type = "CameraID";
-            else if (type == "Photog") type = "PhotogID";
+            if (type == "addLaptop") { type = "LaptopID"; }
+            else if (type == "addCamera") { type = "CameraID"; }
+            else if (type == "addPhotog") { type = "PhotogID"; }
+            else if (type == "remLaptop") { type = "LaptopID"; itemID = 0; }
+            else if (type == "remCamera") { type = "CameraID"; itemID = 0; }
+            else if (type == "remPhotog") { type = "PhotogID"; itemID = 0; }
             else { return false; }
 
             using (SQLiteConnection m_dbConnection = new SQLiteConnection(String.Format("Data Source={0};Version=3;datetimeformat=CurrentCulture;", databaseLocation)))
             {
                 SQLiteCommand command = m_dbConnection.CreateCommand();
-                command.CommandText = String.Format("UPDATE Kits SET {0}=@newID WHERE KitID=@KitID", type);
+                command.CommandText = String.Format("UPDATE Kits SET {0}=@newID WHERE KitID=@KitID; INSERT INTO Repairs ({0}, KitID, Date, Fixed, FixedDate, Notes) VALUES (@oldID, @KitID, \"{1}\", 1, \"{1}\", \"{2}\");", 
+                    type,
+                    DateTime.Now,
+                    itemID > 0 ? String.Format("{0} added to kit.", type) : String.Format("{0} removed from kit.", type));
                 command.Parameters.Add(new SQLiteParameter("@newID", itemID));
                 command.Parameters.Add(new SQLiteParameter("@KitID", kitID));
+                command.Parameters.Add(new SQLiteParameter("@oldID", oldID));
                 m_dbConnection.Open();
-                return command.ExecuteNonQuery() == 1;
+                return command.ExecuteNonQuery() > 0;
             }
         }
     }
