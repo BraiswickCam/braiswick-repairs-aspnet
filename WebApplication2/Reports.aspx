8e529ac55f52e96708dade9a9c1b981fafb80541
Alex <alexbass666@gmail.com>
2016-10-26 12:11:04 +0100

    Add Reports page. Add RepairCost report

---
+++ WebApplication2/Reports.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Reports.aspx.cs" Inherits="WebApplication2.Reports" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <style>
+        * {
+            font-family: Ubuntu, sans-serif;
+        }
+    </style>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <asp:GridView ID="resultsGrid" runat="server"></asp:GridView>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ WebApplication2/Reports.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SQLite;
+using System.Data;
+using System.Text;
+
+namespace WebApplication2
+{
+    public partial class Reports : System.Web.UI.Page
+    {
+        SQLiteConnection m_dbConnection = new SQLiteConnection(String.Format("Data Source={0};Version=3;datetimeformat=CurrentCulture;", "C:\\datatest\\2016repairhistory.sqlite"));
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["rep"] == "repairCost")
+            {
+                BindData(RepairCost());
+            }
+
+        }
+
+        protected DataTable RepairCost()
+        {
+            using (m_dbConnection)
+            {
+                SQLiteCommand command = m_dbConnection.CreateCommand();
+                command.CommandText = "SELECT Photographers.ID, Photographers.Name, Photographers.Initials, Photographers.Active, Photographers.Office, sum(Repairs.RepairCost) AS \"Total Repair Cost\" FROM Photographers LEFT JOIN Repairs ON Photographers.ID = Repairs.PhotogID GROUP BY Photographers.ID ORDER BY \"Total Repair Cost\" DESC";
+                using (SQLiteDataAdapter sda = new SQLiteDataAdapter())
+                {
+                    sda.SelectCommand = command;
+                    using (DataTable dt = new DataTable())
+                    {
+                        sda.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+
+        protected void BindData(DataTable dt)
+        {
+            resultsGrid.DataSource = dt;
+            resultsGrid.DataBind();
+        }
+    }
+}
