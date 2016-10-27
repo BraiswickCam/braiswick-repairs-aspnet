1ec16000216e2cead3d4909293cb5089d57b5f57
Alex <alexbass666@gmail.com>
2016-10-27 10:22:48 +0100

    Update Laptops layout HTML/CSS

--- WebApplication2/Laptops.aspx.designer.cs
+++ WebApplication2/Laptops.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace WebApplication2 {
     public partial class Laptops {
         
         /// <summary>
+        /// mainPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl mainPanel;
+        
+        /// <summary>
         /// idLabel control.
         /// </summary>
         /// <remarks>
