6bcf9956ffa892d174e65dbee04ecf4f2598d0cc
walrusdunne <alexbass666@gmail.com>
2018-05-15 15:08:32 +0100

    Add basic weekly summary

---
+++ WebApplication2/WeeklySummaries.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WeeklySummaries.aspx.cs" Inherits="WebApplication2.WeeklySummaries" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="headextra" runat="server">
+    <style>
+        .top10 {
+            margin-top: 10px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:UpdatePanel runat="server">
+        <ContentTemplate>
+            <div class="row top10">
+                <div class="panel panel-success">
+                    <div class="panel-heading"></div>
+                    <div class="panel-body">
+                        <asp:LinkButton runat="server" ID="thisWeekFeedbackButton" OnClick="thisWeekFeedbackButton_Click">This Week Feedback</asp:LinkButton>
+                    </div>
+                </div>
+            </div>
+            <div class="row top10">
+                <asp:GridView runat="server" ID="printTable" GridLines="None" CssClass="table table-striped table-hover" OnRowDataBound="printTable_RowDataBound"></asp:GridView>
+            </div>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
---
+++ WebApplication2/WeeklySummaries.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SQLite;
+using System.Data;
+using System.Text;
+
+namespace WebApplication2
+{
+    public partial class WeeklySummaries : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LoadWeeklyFeedback()
+        {
+            DateTime startDate = DateTime.Now.StartOfWeek(DayOfWeek.Monday);
+            DateTime endDate = startDate.AddDays(7);
+            printTable.DataSource = GetWeeklyFeedback(startDate, endDate);
+            printTable.DataBind();
+        }
+
+        protected DataTable GetWeeklyFeedback(DateTime weekStart, DateTime weekEnd)
+        {
+            using (SQLiteConnection m_dbConnection = new SQLiteConnection(String.Format("Data Source={0};Version=3;datetimeformat=CurrentCulture;", GlobalVars.dbLocation)))
+            {
+                SQLiteCommand command = m_dbConnection.CreateCommand();
+                command.CommandText = "SELECT PReports.ID, PReports.Date, PReports.Office, PReports.Job, PReports.School, PReports.Type, PReports.Cost, PReports.Photographer, Photographers.Initials, Photographers.Name, PReports.Status, PReports.Notes FROM PReports LEFT JOIN Photographers ON PReports.Photographer = Photographers.ID WHERE PReports.Status = 'FEEDBACK' AND DateCreated >= @StartWeek AND DateCreated < @EndWeek";
+                command.Parameters.Add(new SQLiteParameter("@StartWeek", weekStart.ToString("yyyy-MM-dd")));
+                command.Parameters.Add(new SQLiteParameter("@EndWeek", weekEnd.ToString("yyyy-MM-dd")));
+                using (SQLiteDataAdapter sda = new SQLiteDataAdapter())
+                {
+                    sda.SelectCommand = command;
+                    using (DataTable dt = new DataTable())
+                    {
+                        sda.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+
+        protected void thisWeekFeedbackButton_Click(object sender, EventArgs e)
+        {
+            LoadWeeklyFeedback();
+        }
+
+        protected void printTable_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+        }
+    }
+
+    public static class DateTimeExtensions
+    {
+        public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek)
+        {
+            int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
+            return dt.AddDays(-1 * diff).Date;
+        }
+    }
+}
