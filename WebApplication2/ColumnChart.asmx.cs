8c56ac32ceadcaaee690521ac1304ad00486e69e
walrusdunne <alexbass666@gmail.com>
2018-11-21 14:52:11 +0000

    Add web service for reports data

--- WebApplication2/ColumnChart.asmx.cs
+++ WebApplication2/ColumnChart.asmx.cs
@@ -6,6 +6,7 @@ using System.Web.Services;
 using System.Data;
 using System.Data.SQLite;
 using System.Web.Script.Services;
+using System.Web.Script.Serialization;
 
 namespace WebApplication2
 {
@@ -82,6 +83,34 @@ namespace WebApplication2
             return results;
         }
 
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        [WebMethod]
+        //[System.Xml.Serialization.XmlInclude(typeof(PReportRow))]
+        public List<object> GetReports()
+        {
+            DataTable dt = QueryDatabase("SELECT PReports.ID, PReports.Date, PReports.Office, PReports.Job, PReports.School, PReports.Type, PReports.Cost, PReports.Photographer, Photographers.Initials, Photographers.Name, PReports.Status, PReports.Notes FROM PReports LEFT JOIN Photographers ON PReports.Photographer = Photographers.ID ORDER BY PReports.ID DESC");
+            List<object> results = new List<object>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                PReportRow prow = new PReportRow();
+                prow.ID = Convert.ToInt32(dr[0]);
+                prow.Date = dr[1].ToString();
+                prow.Office = dr[2].ToString();
+                prow.Job = dr[3].ToString();
+                prow.School = dr[4].ToString();
+                prow.Type = dr[5].ToString();
+                prow.Cost = Convert.ToDecimal(dr[6]);
+                prow.Photographer = dr[7].ToString();
+                prow.Initials = dr[8].ToString();
+                prow.Name = dr[9].ToString();
+                prow.Status = dr[10].ToString();
+                prow.Notes = dr[11].ToString();
+                results.Add(prow);
+            }
+            return results;
+            //return new JavaScriptSerializer().Serialize(results);
+        }
+
         public static DataTable QueryDatabase(string query)
         {
             using (SQLiteConnection m_dbConnection = new SQLiteConnection(String.Format("Data Source={0};Version=3;datetimeformat=CurrentCulture;", GlobalVars.dbLocation)))
@@ -98,6 +127,22 @@ namespace WebApplication2
                     }
                 }
             }
+        }
+
+        public class PReportRow
+        {
+            public int ID;
+            public string Date;
+            public string Office;
+            public string Job;
+            public string School;
+            public string Type;
+            public decimal Cost;
+            public string Photographer;
+            public string Initials;
+            public string Name;
+            public string Status;
+            public string Notes;
         }
 
         public class ChartDetails
